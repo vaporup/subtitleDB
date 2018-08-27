c0fbb0e8b9a136a36634ea65b2eba7fbfae3037b
Sven Wick <sven.wick@gmx.de>
2018-08-27 15:55:02 +0200

    more work on indexer

--- tools/index.py
+++ tools/index.py
@@ -23,7 +23,7 @@ for root, dirnames, filenames in os.walk('subtitles'):
                 #print match["hash"][0:2] + "/" + match["hash"][2:-1] + "-" + match["filesize"], yaml_content["language"], yaml_content["title"], yaml_path
                 #print match["hash"][0:2] + "/" + match["hash"][2:-1], root.replace("subtitles/",""), yaml_content["title"]
                 #print match["hash"][0:2] + "/" + match["hash"][2:-1] + "-" + match["filesize"] + "-" + yaml_content["language"] + "-" + root.replace("subtitles/","")
-                directory = "/tmp/index/" + match["hash"][0:2] + "/" + match["hash"][2:-1] + "-" + match["filesize"]
+                directory = "/tmp/index/by-hash-filesize/" + match["hash"][0:2] + "/" + match["hash"][2:-1] + "-" + match["filesize"]
                 sub = root.replace("subtitles/","")
                 if not os.path.exists(directory):
                   os.makedirs(directory)
@@ -31,7 +31,7 @@ for root, dirnames, filenames in os.walk('subtitles'):
                 filename = directory + "/" + yaml_content["language"]
 
                 if os.path.exists(filename):
-                  with open("filename", "r+") as f:
+                  with open(filename, "r+") as f:
                     for line in f:
                       if sub in line:
                         break
@@ -41,3 +41,45 @@ for root, dirnames, filenames in os.walk('subtitles'):
                   f = open(filename, "w")
                   f.write(sub + '\n')
                   f.close()
+
+          if "imdb_id" in yaml_content:
+            directory = "/tmp/index/by-imdb/" + yaml_content["imdb_id"]
+            sub = root.replace("subtitles/","")
+
+            if not os.path.exists(directory):
+              os.makedirs(directory)
+
+            filename = directory + "/" + yaml_content["language"]
+
+            if os.path.exists(filename):
+              with open(filename, "r+") as f:
+                for line in f:
+                  if sub in line:
+                    break
+                else: # not found, we are at the eof
+                  f.write(sub) # append missing data
+            else:
+              f = open(filename, "w")
+              f.write(sub + '\n')
+              f.close()
+
+          if "tmdb_id" in yaml_content:
+            directory = "/tmp/index/by-tmdb/" + yaml_content["tmdb_id"]
+            sub = root.replace("subtitles/","")
+
+            if not os.path.exists(directory):
+              os.makedirs(directory)
+
+            filename = directory + "/" + yaml_content["language"]
+
+            if os.path.exists(filename):
+              with open(filename, "r+") as f:
+                for line in f:
+                  if sub in line:
+                    break
+                else: # not found, we are at the eof
+                  f.write(sub) # append missing data
+            else:
+              f = open(filename, "w")
+              f.write(sub + '\n')
+              f.close()
