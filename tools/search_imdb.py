35785ab3f82ae35a4ac31f282be27f5aee4bac31
nutzer12 <nutzer12@notebook12.linuxhotel.de>
2019-04-03 22:54:38 +0200

    Add AKAs

--- tools/search_imdb.py
+++ tools/search_imdb.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import json
 import sys
 import urllib2
 
@@ -49,24 +48,54 @@ for data in data_raw:
 
         URL = "https://www.imdb.com%s" % foo[chosen]['url'] 
 
+        URL_AKAS = URL + "releaseinfo"
+
+
         oururl= urllib2.urlopen(URL).read()
         soup = BeautifulSoup(oururl, 'lxml')
 
-        data = json.loads(soup.find('script', type='application/ld+json').text)
+        oururl2= urllib2.urlopen(URL_AKAS).read()
+        soup2 = BeautifulSoup(oururl2, 'lxml')
 
-        print "title: ".ljust(20)   , data['name']
+	# <td class="aka-item__title">
 
-        if data.has_key('datePublished'):
-            print "year: ".ljust(20)    , data['datePublished'].split("-")[0]
+	akas = soup2.find_all('td', class_='aka-item__title')
 
-        if data.has_key('type'):
-            print "type: ".ljust(20) , data['@type']
+	bla = set(akas)
+
+	# <div class="originalTitle">
+	orig_title_found = soup.find('div', class_='originalTitle')
+        if orig_title_found:
+          orig_title = orig_title_found.contents[0]
+        else:
+          orig_title = ""
+
+	# <span id="titleYear">
+	year_found = soup.find('span', id='titleYear')
+        if year_found:
+          year = year_found.text
+        else:
+          year = ""
+
+	# <meta property='og:title' content="Devdas (2002) - IMDb" />
+	title = soup.find('meta', property='og:title')
+
+	#  <div class="summary_text">
+	summary = soup.find('div', class_='summary_text').contents[0].strip()
+
+        print "title: ".ljust(20), title['content'].replace(" - IMDb", "")
+        print "title_orig: ".ljust(20) , orig_title
+
+
+        print "year: ".ljust(20)    , year.strip("(").strip(")")
 
         print "url: ".ljust(20) , URL
         print "id: ".ljust(20) , URL.split('/')[-2]
 
-        if data.has_key('description'):
-            print "description: ".ljust(20), data['description']
+	for aka in bla:
+	  print "title_aka: ".ljust(20), aka.text
+
+        print "summary: ".ljust(20), summary
 
         print
         sys.exit()
