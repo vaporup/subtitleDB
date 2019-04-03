845e0a86d57c228f84baa4de629629b606adcaea
nutzer12 <nutzer12@notebook12.linuxhotel.de>
2019-04-03 20:12:39 +0200

    Search IMDB prototype

---
+++ tools/search_imdb.py
@@ -0,0 +1,63 @@
+import json
+import urllib2
+
+from bs4 import BeautifulSoup
+
+URL = 'https://www.imdb.com/find?q=matrix&s=tt'
+oururl= urllib2.urlopen(URL).read()
+soup = BeautifulSoup(oururl, 'lxml')
+
+foo = {}
+
+data_raw = soup.find_all('td', class_='result_text')
+
+for data in data_raw:
+  index = data_raw.index(data)
+
+  foo[index] = {}
+
+  url   = data.find('a')['href'].split('?')[0]
+  title = data.find('a').contents[0]
+
+  foo[index]['url'] = url
+  foo[index]['title'] = title
+
+  small =  data.find('span', class_='ghost')
+
+  if small:
+    foo[index]['descr'] = data.contents[2]
+    print index, url, title, data.contents[2]
+  else:
+    foo[index]['descr'] = data.contents[-1]
+    print index, url, title, data.contents[-1]
+
+print
+
+chosen = input("Select the input: ")
+
+print
+
+URL = "https://www.imdb.com%s" % foo[chosen]['url'] 
+
+
+oururl= urllib2.urlopen(URL).read()
+soup = BeautifulSoup(oururl, 'lxml')
+
+data = json.loads(soup.find('script', type='application/ld+json').text)
+
+print "title :"   , data['name']
+
+if data.has_key('datePublished'):
+  print "year :"    , data['datePublished'].split("-")[0]
+
+
+if data.has_key('type'):
+  print "type :" , data['@type']
+
+print "url :" , URL
+print "id :" , URL.split('/')[-2]
+
+if data.has_key('description'):
+  print "description :", data['description']
+
+print
