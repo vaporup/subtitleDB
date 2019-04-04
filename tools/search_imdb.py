035ff3557bb452a85193bf7f07846d5b2443e569
nutzer12 <nutzer12@notebook12.linuxhotel.de>
2019-04-04 14:08:05 +0200

    continue asking

--- tools/search_imdb.py
+++ tools/search_imdb.py
@@ -19,88 +19,89 @@ print
 print "%s result(s) - Just press ENTER to page trough results" % len(data_raw)
 print
 
-for data in data_raw:
+while True:
 
-  index = data_raw.index(data) + 1
+    for data in data_raw:
 
-  foo[index] = {}
+      index = data_raw.index(data) + 1
 
-  url   = data.find('a')['href'].split('?')[0]
-  title = data.find('a').contents[0]
+      foo[index] = {}
 
-  foo[index]['url'] = url
-  foo[index]['title'] = title
+      url   = data.find('a')['href'].split('?')[0]
+      title = data.find('a').contents[0]
 
-  small =  data.find('span', class_='ghost')
+      foo[index]['url'] = url
+      foo[index]['title'] = title
 
-  if small:
-    foo[index]['descr'] = data.contents[2]
-    print index, title, data.contents[2]
-  else:
-    foo[index]['descr'] = data.contents[-1]
-    print index, title, data.contents[-1]
+      small =  data.find('span', class_='ghost')
 
-  if index % 10 == 0 or index == len(data_raw):
-    try:
-        print
-        chosen = input("Select the input: ")
-        print
+      if small:
+        foo[index]['descr'] = data.contents[2]
+        print index, title, data.contents[2]
+      else:
+        foo[index]['descr'] = data.contents[-1]
+        print index, title, data.contents[-1]
 
-        URL = "https://www.imdb.com%s" % foo[chosen]['url'] 
+      if index % 10 == 0 or index == len(data_raw):
+        try:
+            print
+            chosen = input("Select the input: ")
+            print
 
-        URL_AKAS = URL + "releaseinfo"
+            URL = "https://www.imdb.com%s" % foo[chosen]['url'] 
 
-
-        oururl= urllib2.urlopen(URL).read()
-        soup = BeautifulSoup(oururl, 'lxml')
-
-        oururl2= urllib2.urlopen(URL_AKAS).read()
-        soup2 = BeautifulSoup(oururl2, 'lxml')
-
-	# <td class="aka-item__title">
-
-	akas = soup2.find_all('td', class_='aka-item__title')
-
-	bla = set(akas)
-
-	# <div class="originalTitle">
-	orig_title_found = soup.find('div', class_='originalTitle')
-        if orig_title_found:
-          orig_title = orig_title_found.contents[0]
-        else:
-          orig_title = ""
-
-	# <span id="titleYear">
-	year_found = soup.find('span', id='titleYear')
-        if year_found:
-          year = year_found.text
-        else:
-          year = ""
-
-	# <meta property='og:title' content="Devdas (2002) - IMDb" />
-	title = soup.find('meta', property='og:title')
-
-	#  <div class="summary_text">
-	summary = soup.find('div', class_='summary_text').contents[0].strip()
-
-        print "title: ".ljust(20), title['content'].replace(" - IMDb", "")
-        print "title_orig: ".ljust(20) , orig_title
+            URL_AKAS = URL + "releaseinfo"
 
 
-        print "year: ".ljust(20)    , year.strip("(").strip(")")
+            oururl= urllib2.urlopen(URL).read()
+            soup = BeautifulSoup(oururl, 'lxml')
 
-        print "url: ".ljust(20) , URL
-        print "id: ".ljust(20) , URL.split('/')[-2]
+            oururl2= urllib2.urlopen(URL_AKAS).read()
+            soup2 = BeautifulSoup(oururl2, 'lxml')
 
-	for aka in bla:
-	  print "title_aka: ".ljust(20), aka.text
+            # <td class="aka-item__title">
 
-        print "summary: ".ljust(20), summary
+            akas = soup2.find_all('td', class_='aka-item__title')
 
-        print
-        sys.exit()
+            bla = set(akas)
 
-    except SyntaxError:
-        print
-        continue
+            # <div class="originalTitle">
+            orig_title_found = soup.find('div', class_='originalTitle')
+            if orig_title_found:
+              orig_title = orig_title_found.contents[0]
+            else:
+              orig_title = ""
+
+            # <span id="titleYear">
+            year_found = soup.find('span', id='titleYear')
+            if year_found:
+              year = year_found.text
+            else:
+              year = ""
+
+            # <meta property='og:title' content="Devdas (2002) - IMDb" />
+            title = soup.find('meta', property='og:title')
+
+            #  <div class="summary_text">
+            summary = soup.find('div', class_='summary_text').contents[0].strip()
+
+            print "title: ".ljust(20), title['content'].replace(" - IMDb", "")
+            print "title_orig: ".ljust(20) , orig_title
+
+
+            print "year: ".ljust(20)    , year.strip("(").strip(")")
+
+            print "url: ".ljust(20) , URL
+            print "id: ".ljust(20) , URL.split('/')[-2]
+
+            for aka in bla:
+              print "title_aka: ".ljust(20), aka.text
+
+            print "summary: ".ljust(20), summary
+
+            print
+
+        except SyntaxError:
+            print
+            continue
 
